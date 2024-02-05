df607492509e078abdcd0d090cb0a12bbf0b4a69
vinay10082 <20bph043@nith.ac.in>
2024-02-05 15:46:54 +0530

    lec 2 completed

--- day-1_basic_recursion.cpp
+++ day-1_basic_recursion.cpp
@@ -4,7 +4,7 @@ using namespace std;
 //global counter
 int cnt = 0;
 
-// print name 5 times
+// print name 5 times using recursion
 void printName5times(string name, int times){
 
 //base condition
@@ -17,13 +17,29 @@ printName5times(name,times);
 }
 
 // print 1 to N using backtracking
-void print1toN(){
+void print1toN(int N){
 
+if(N<1) return;
+
+print1toN(N-1);
+cout<<N<<endl;
+}
+
+// print N to 1 using backtracking
+void printNto1(int s, int N){
+
+if(s>N) return;
+
+printNto1(s+1,N);
+
+cout<<s<<endl;
 }
 
 int main(){
 
     // printName5times("vinay", 5);
+    // print1toN(3);
+    printNto1(1,3);
 
     return 0;
 }
